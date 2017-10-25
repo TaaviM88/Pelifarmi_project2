2296099970082f3f2f58f1599da29779ab0088f6
Lauri Syvänen <lauri.syvanen@courses.tamk.fi>
2017-10-25 14:52:48 +0300

    Ääni autolle ja muita pieniä tweekkauksia koodeihin.

---
+++ Project 2/Assets/Scripts/CarSound.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CarSound : MonoBehaviour {
+
+
+	private AudioSource source;
+	public AudioClip _carSound;
+
+
+	void Start()
+	{
+		source = GetComponent<AudioSource> ();
+	}
+
+	void Update() 
+	{
+		
+		if(Input.GetKeyDown(KeyCode.W))
+		{   
+			source.Play();
+			//AudioSource.Play(44100);
+		}
+		if(Input.GetKeyUp(KeyCode.W))
+		{
+			source.Stop();
+		}
+			
+			
+	}
+}
